0709d99665062a2168735857303f85b5d62d3992
brad <contact@yobson.xyz>
2025-09-27 03:49:59 +0100

    use static env for db

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import Database from 'bun:sqlite';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
+import { DATABASE_URL } from '$env/static/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = new Database(env.DATABASE_URL);
+const client = new Database(DATABASE_URL);
 
 export const db = drizzle(client, { schema });
