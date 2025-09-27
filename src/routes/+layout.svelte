31ddae77c14d40707c3b9b31bf9a555c872710b3
brad <contact@yobson.xyz>
2025-09-27 04:03:52 +0100

    add dark mode switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
 	import Nav from '$lib/components/Nav.svelte';
-	
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+<ModeWatcher />
 
 <header>
 	<Nav />
