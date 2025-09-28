2fd7d0e3395ebcf77d7182feb866e20cbcbd3452
brad <contact@yobson.xyz>
2025-09-28 23:14:47 +0100

    svelte/no-navigation-without-resolve ignoreLinks

--- eslint.config.js
+++ eslint.config.js
@@ -37,5 +37,16 @@ export default defineConfig(
 				svelteConfig
 			}
 		}
+	},
+	{
+		'svelte/no-navigation-without-resolve': [
+			'error',
+			{
+				ignoreGoto: false,
+				ignoreLinks: true,
+				ignorePushState: false,
+				ignoreReplaceState: false
+			}
+		]
 	}
 );
